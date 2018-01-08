c92ef427390b08d509ae40cf01a89adb9a403f13
What <loujin_007@126.com>
2018-01-08 17:48:00 +0800

    first init

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+//增加新路由的方法
+router.get('/hello',function(req,res,next){
+    res.render('index', { title: 'Hello' });
+});
+
+module.exports = router;
